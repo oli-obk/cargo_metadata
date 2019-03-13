aeac54fc4f65ae3e0a674ac85889bc61e8ba58f2
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2019-03-13 17:28:59 +0100

    Fix #74

--- tests/test_samples.rs
+++ tests/test_samples.rs
@@ -125,9 +125,9 @@ fn cargo_version() -> semver::Version {
         .arg("-V")
         .output()
         .expect("Failed to exec cargo.");
-    let out = std::str::from_utf8(&output.stdout).expect("invalid utf8");
+    let out = std::str::from_utf8(&output.stdout).expect("invalid utf8").trim();
     let split: Vec<&str> = out.split_whitespace().collect();
-    assert!(split.len() > 2, "cargo -V output is unexpected: {}", out);
+    assert!(split.len() >= 2, "cargo -V output is unexpected: {}", out);
     semver::Version::parse(split[1]).expect("cargo -V semver could not be parsed")
 }
 
