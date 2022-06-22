3f131a38a40b5f41df9041ee0a8a3dd9e9bd1fb0
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-06-22 14:27:15 +0000

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -658,7 +658,7 @@ impl MetadataCommand {
         let stdout = from_utf8(&output.stdout)?
             .lines()
             .find(|line| line.starts_with('{'))
-            .ok_or_else(|| Error::NoJson)?;
+            .ok_or(Error::NoJson)?;
         Self::parse(stdout)
     }
 }
