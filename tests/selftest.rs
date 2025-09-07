d64b6e00d53320bd2aeba1393b4832842346d1cf
Samuel Moelius <sam@moeli.us>
2025-09-07 12:10:06 -0400

    Proposed `-Zbuild-dir` fix

--- tests/selftest.rs
+++ tests/selftest.rs
@@ -181,23 +181,11 @@ fn workspace_default_packages() {
 #[test]
 #[cfg(feature = "unstable")]
 fn build_dir() {
-    let metadata = MetadataCommand::new()
-        .no_deps()
-        .other_options(["-Zbuild-dir"].map(str::to_string))
-        .exec()
-        .unwrap();
+    let metadata = MetadataCommand::new().no_deps().exec().unwrap();
 
     assert!(&metadata.build_directory.is_some());
     assert!(&metadata
         .build_directory
         .unwrap()
         .ends_with("cargo_metadata/target"));
-}
-
-#[test]
-#[cfg(feature = "unstable")]
-fn build_dir_disabled() {
-    let metadata = MetadataCommand::new().no_deps().exec().unwrap();
-
-    assert!(&metadata.build_directory.is_none());
 }
